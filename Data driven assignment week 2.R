f7110016c481a9c14828e1070dc62cba2743f32b
Giangdao2020 <78479279+Giangdao2020@users.noreply.github.com>
2021-02-24 09:45:34 +0100

    Add files via upload

---
+++ Data driven assignment week 2.R
@@ -0,0 +1,26 @@
+---
+title: "Assigment - Data Driven Decision Making"
+author: Giang Dao
+date: "`r format(Sys.time(), '%d %B, %Y')`"
+self_contained: false
+output: html_notebook
+---
+
+Business case on how Amazon uses big data for their recommendation system.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/S4RL6prqtGQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen data-external="1"></iframe>
+
+## Why was data driven decision making (3DM) useful in this business case?
+
+Data-driven decision-making (DDD) refers to the practice of basing decisions on the
+analysis of data, rather than purely on intuition (Provost & Fawcett, 2013). In this case, Amazon has made use of big data in understanding the activities and purchasing patterns of customers. The online platform then analyzes these data to form an algorithm that makes suggestions to customers with items that are most close to the customers' interest.
+
+## How did the organization apply 3DM?
+
+As businesses are collecting more data
+than required for any use (McAfee et al., 2012), Amazon also follows this trend in their process of applying data-driven decision making. Amazon records all actions that customers perform on their website, ranging from viewing an item, rating and adding the item in their shopping carts. With such a large amount of data collected from customer behavior, Amazon is able to recommend items a customer might be interested in through comparing all visitors' choices or personalized user-based information.
+
+## What can the organization do with 3DM that they couldn’t do before?
+
+With the increasing trend of customer-centric approach, understanding customers' wants and needs will enable businesses to make additional suggestions of related products that would stoke their interest (Newman, 2016). Through recording and analyzing customer behavior as well as taking the personalized interest into consideration, Amazon has developed a more efficient recommendation system. The system shows items that customers are more likely to purchase and provide better shopping experience without traditional surveys offered to customers, which could be of inconvenience.
+As their use of data provides a better understanding of users' behavior and interest, Amazon is able to generate more sales and profit through offering visitors' a personalized experience.
